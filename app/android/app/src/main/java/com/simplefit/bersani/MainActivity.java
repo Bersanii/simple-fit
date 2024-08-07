96588f9cd657ce25aaf541ef3f86f9cf35b6a035
Vitor Bersani <vitor.bersani@mutant.com.br>
2024-08-07 15:58:47 -0300

    capacitor added

---
+++ app/android/app/src/main/java/com/simplefit/bersani/MainActivity.java
@@ -0,0 +1,5 @@
+package com.simplefit.bersani;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.simplefit.bersani',
+  appName: 'simple-fit',
+  webDir: 'dist'
+};
+
+export default config;
