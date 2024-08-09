1976a0a18e787dd27fb907f0e30f16fe878aa5b4
Vitor Bersani <vitor.bersani@mutant.com.br>
2024-08-09 14:43:32 -0300

    fixing entities and migrations import

--- app/src/databases/datasources/SimplefitDataSource.ts
+++ app/src/databases/datasources/SimplefitDataSource.ts
@@ -1,8 +1,8 @@
 //https://github.com/capacitor-community/sqlite/blob/master/docs/TypeORM-Usage-From-5.6.0.md
 import { DataSource , type DataSourceOptions} from 'typeorm';
 import sqliteParams from '../sqliteParams';
-import * as entities from '../entities';
-import * as migrations from '../migrations';
+// import * as entities from '../entities';
+// import * as migrations from '../migrations';
 
 const dbName = "DB_SIMPLEFIT";
 
@@ -12,8 +12,9 @@ const dataSourceConfig: DataSourceOptions = {
   driver: sqliteParams.connection,
   database: dbName,
   mode: 'no-encryption',
-  entities: entities,
-  migrations: migrations, //["../migrations/author/*{.ts,.js}"] pra isso funcionar esbuild.minifyIdentifiers deve estar como false, se não o esbuild vai cagar o nome da classe, e se o nome não estiver correto ele não importa
+  entities: ['../entities/*.ts'],
+  // migrations: migrations, //["../migrations/author/*{.ts,.js}"] pra isso funcionar esbuild.minifyIdentifiers deve estar como false, se não o esbuild vai cagar o nome da classe, e se o nome não estiver correto ele não importa
+  migrations: ['../migrations/*.ts'],
   subscribers: [],
   logging: [/*'query',*/ 'error','schema'],
   synchronize: false,     // !!!You will lose all data in database if set to `true`
