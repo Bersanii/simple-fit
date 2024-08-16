5ea9042cc8534426dc387de81b7b3e0214260f72
Vitor Bersani <vitor.bersani@mutant.com.br>
2024-08-15 23:53:18 -0300

    picker avançado

--- app/src/components/PickerExercicio.vue
+++ app/src/components/PickerExercicio.vue
@@ -1,37 +1,82 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref, onBeforeMount } from 'vue'
 import dataSourceSimpleFit from '@/databases/datasources/SimplefitDataSource';
 import { EspecificacaoExercicio } from '@/databases/entities';
 
-const model = defineModel({ type: Boolean })
+const opened = ref(false)
+let callback: (x: EspecificacaoExercicio) => any = () => { }
+let templatesExerciciosOriginal: EspecificacaoExercicio[] = []
+let grupamentos: string[] = []
+const templatesExercicios = ref<EspecificacaoExercicio[]>([])
+const filter = ref({
+    nome: '',
+    grupamento: ''
+})
 
-const emit = defineEmits<{
-  (e: 'change', exercicio: EspecificacaoExercicio): void
-//   (e: 'update', value: string): void
-}>()
-
-function teste() {
-    // emit('change', )
+function open(x: typeof callback) {
+    callback = x
+    opened.value = true
 }
 
+function selectEspecificacao(especificacao: EspecificacaoExercicio) {
+    opened.value = false
+    const deepclone: EspecificacaoExercicio = JSON.parse(JSON.stringify(especificacao))
+    callback(deepclone)
+}
+
+function filtrar() {
+    templatesExercicios.value = templatesExerciciosOriginal.filter(x => x.nome.includes(filter.value.nome) && (filter.value.grupamento == '' || x.grupamento == filter.value.grupamento))
+}
+
+onBeforeMount(async () => {
+    console.log('before mount')
+
+    const especificacaoExerciciorepository = dataSourceSimpleFit.getRepository(EspecificacaoExercicio)
+    templatesExerciciosOriginal = await especificacaoExerciciorepository.find()
+    templatesExercicios.value = templatesExerciciosOriginal
+
+    grupamentos = templatesExerciciosOriginal.map(x => x.grupamento).filter(function (item, pos, self) {
+        return self.indexOf(item) == pos
+    })
+    console.log(grupamentos)
+})
+
+defineExpose({
+    open
+})
 
 </script>
 
 <template>
-    <v-dialog v-model="model" transition="dialog-bottom-transition" fullscreen>
+    <v-dialog v-model="opened" transition="dialog-bottom-transition" fullscreen>
         <!-- <template v-slot:activator="{ props: activatorProps }">
             <v-btn prepend-icon="mdi-cog" size="small" text="Settings" v-bind="activatorProps"></v-btn>
         </template> -->
 
-        <v-card class="mt-5 rounded-lg" color="grey-darken-4">
+        <v-card class="mt-10 rounded-lg" color="grey-darken-4">
             <v-toolbar color="grey-darken-3">
-                <v-btn icon="mdi-close" @click="model = false"></v-btn>
+                <v-btn icon="mdi-close" @click="opened = false"></v-btn>
 
                 <v-toolbar-title>Selecione um Exercicio</v-toolbar-title>
             </v-toolbar>
 
-            <div class="bg-orange h-100" style="overflow-y: auto;">
-               
+            <div class="pa-2 d-flex">
+                <v-text-field v-model="filter.nome" label="Nome" variant="solo-filled" density="comfortable"
+                    color="orange" bg-color="grey-darken-3" hide-details @update:model-value="filtrar" />
+                <v-select class="ml-2" v-model="filter.grupamento" label="Grupamento" :items="grupamentos"
+                    variant="solo-filled" hide-details density="comfortable" bg-color="grey-darken-3" @update:model-value="filtrar" />
+            </div>
+
+            <div class="px-2 pb-10" style="overflow-y: auto;">
+                <div class="mb-2" v-for="templateExercicio in templatesExercicios"
+                    @click="selectEspecificacao(templateExercicio)">
+                    <div>
+                        {{ templateExercicio.nome }}
+                    </div>
+                    <div class="text-caption">
+                        {{ templateExercicio.grupamento }}
+                    </div>
+                </div>
             </div>
         </v-card>
     </v-dialog>
--- app/src/pages/NovoTreino.vue
+++ app/src/pages/NovoTreino.vue
@@ -11,62 +11,59 @@ import PickerExercicio from '@/components/PickerExercicio.vue';
 //     PickerExercicioComponent.open()
 // }
 
-const PickerExercicioComponent = ref(false)
+const pickerExercicio = ref<InstanceType<typeof PickerExercicio> | null>(null)
 
 const form = ref({
-    nome: {
-        value: '',
-        rules: [(value: string) => { return !!value ? true : 'Informe um nome para o treino.' }]
-    }
+  nome: {
+    value: '',
+    rules: [(value: string) => { return !!value ? true : 'Informe um nome para o treino.' }]
+  }
 })
 
 function printEmit(exercicio: EspecificacaoExercicio) {
-    console.log(exercicio)
+  console.log(exercicio)
+}
+
+function pickExercicio(){
+  pickerExercicio.value?.open(printEmit)
 }
 
 
 </script>
 
 <template>
-    <PickerExercicio v-model="PickerExercicioComponent" @change="printEmit" />
+  <PickerExercicio ref="pickerExercicio" />
 
-    <div class="h-100 pt-2">
-        <v-form @submit.prevent>
-            <div class="d-flex w-100 align-center px-4">
-                <v-btn type="submit" icon="mdi-arrow-left" variant="text"></v-btn>
+  <div class="h-100 pt-2">
+    <v-form @submit.prevent>
+      <div class="d-flex w-100 align-center px-4">
+        <v-btn icon="mdi-arrow-left" variant="text"></v-btn>
 
-                <div class="ml-2 font-weight-black text-h6">Novo Treino</div>
+        <div class="ml-2 font-weight-black text-h6">Novo Treino</div>
 
-                <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
 
-                <v-btn type="submit" icon="mdi-check" variant="text" color="green"></v-btn>
-            </div>
+        <v-btn type="submit" icon="mdi-check" variant="text" color="green"></v-btn>
+      </div>
 
-            <v-divider class="my-2"></v-divider>
+      <v-divider class="my-2"></v-divider>
 
-            <div class="px-5 pt-5">
+      <div class="px-5 pt-5">
 
-                <v-text-field v-model="form.nome.value" :rules="form.nome.rules" label="Nome" variant="outlined"
-                    color="orange"></v-text-field>
-            </div>
+        <v-text-field v-model="form.nome.value" :rules="form.nome.rules" label="Nome" variant="outlined"
+          color="orange"></v-text-field>
 
-        </v-form>
+        <div class="d-flex align-center">
+          <div class="text-h5 ">
+            Exercicios
+          </div>
+          <v-spacer></v-spacer>
+          <v-btn icon="mdi-plus" variant="text" @click="pickExercicio"></v-btn>
+        </div>
+        <v-divider class="my-2"></v-divider>
 
+      </div>
 
-        <!-- <v-carousel class="mt-2">
-            <v-carousel-item v-for="treino in treinos">
-                <v-card variant="elevated" color="grey-darken-3 rounded-lg mx-4 d-flex flex-column"
-                    style="min-width: 50vw;">
-                    <template v-slot:title>
-                        <span class="font-weight-black">{{ treino.nome }}</span>
-                    </template>
-<div class="px-4 py-2 bg-grey-darken-2 flex-grow-1">
-    <div v-for="exercicio in treino.exercicios">
-        {{ exercicio.nome }}
-    </div>
-</div>
-</v-card>
-</v-carousel-item>
-</v-carousel> -->
-    </div>
+    </v-form>
+  </div>
 </template>
